54d1d6f74ed30abef8b578a22caf3bfa5d5cd55e
Travis Fox <travis.fox90@gmail.com>
2020-06-24 02:23:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,6 @@ def reverse_each_word(sentence_to_reverse)
   sentence_with_words_reversed = sentence_in_array_form.each { |word| word.reverse }
   string_with_words_reversed = sentence_with_words_reversed.join(" ")
 end
+
+reverse_each_word(sentence_to_reverse)
+
