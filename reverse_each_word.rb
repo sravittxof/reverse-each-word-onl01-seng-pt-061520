0250ac47d8f12ae20eee1b029ae25c6553334b4e
Travis Fox <travis.fox90@gmail.com>
2020-06-24 02:36:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ sentence_to_reverse = "Reverse each word in this sentence"
 
 def reverse_each_word(sentence_to_reverse)
   sentence_in_array_form = sentence_to_reverse.split(" ")
-  sentence_with_words_reversed = sentence_in_array_form.each { |word| word.reverse }
+  sentence_with_words_reversed = sentence_in_array_form.each { |word| word.reverse! }
   string_with_words_reversed = sentence_with_words_reversed.join(" ")
 end
 
