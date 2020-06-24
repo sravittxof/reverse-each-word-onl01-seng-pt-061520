762bf8e8469c0d9d66c884e31649d77779e6154e
Travis Fox <travis.fox90@gmail.com>
2020-06-24 02:39:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,7 @@ reverse_each_word(sentence_to_reverse)
 
 def reverse_each_word_with_collect(sentence_to_reverse)
   sentence_in_array_form = sentence_to_reverse.split(" ")
-  sentence_with_words_reversed = sentence_in_array_form.collect { |word| word.reverse }
+  sentence_with_words_reversed = sentence_in_array_form.collect { |word| word.reverse! }
   string_with_words_reversed = sentence_with_words_reversed.join(" ")
 end
 
